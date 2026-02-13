3bfffcd5d09a2c5d0b4129ba1a36e386b553d737
Laur <zamfir.laurentiu111@gmail.com>
2026-02-14 01:05:58 +0200

    Added feature where 6b6t is automatically added to the server list if not already there

---
+++ mc-1.21.8/src/main/java/me/itskronx11/anarchymod/mixin/ServerListMixin.java
@@ -0,0 +1,29 @@
+package me.itskronx11.anarchymod.mixin;
+
+import me.itskronx11.anarchymod.Domains;
+import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.client.multiplayer.ServerList;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(ServerList.class)
+public class ServerListMixin {
+
+    @Shadow
+    @Final
+    private List<ServerData> serverList;
+
+    @Inject(method = "load", at = @At("TAIL"))
+    public void afterLoad(CallbackInfo ci) {
+        if (serverList.stream().noneMatch(data -> Domains.contains(data.ip))) {
+            serverList.addFirst(new ServerData("6b6t", "6b6t.org", ServerData.Type.OTHER));
+        }
+    }
+
+}
